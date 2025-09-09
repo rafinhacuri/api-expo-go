9e29f402b1a2d56c628dbaf39c6289dae1885873
Rafael Curi Leonardo <rafaelcuri@cbpf.br>
2025-09-09 14:33:44 -0300

    ✨ creat crypt password

---
+++ passwords/crypt.go
@@ -0,0 +1,23 @@
+package passwords
+
+import (
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func BCrypt(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return "{BCRYPT}" + string(hashed), nil
+}
+
+func VerifyBCrypt(password, hashed string) bool {
+	hashed = strings.TrimPrefix(hashed, "{BCRYPT}")
+	if strings.HasPrefix(hashed, "{CRYPT}") {
+		return false
+	}
+	return bcrypt.CompareHashAndPassword([]byte(password), []byte(hashed)) == nil
+}
