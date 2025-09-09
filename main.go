9034227f95393c3eabfdf3dc1d21c596719ffbf3
Rafael Curi Leonardo <rafaelcuri@cbpf.br>
2025-09-09 14:32:43 -0300

    ✨ creat routes

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/rafinhacuri/api-expo-go.git/db"
+	"github.com/rafinhacuri/api-expo-go.git/routes"
 )
 
+func init() {
+	godotenv.Load()
+
+	if err := db.InitDB(true, os.Getenv("MONGO_USERNAME"), os.Getenv("MONGO_URL"), os.Getenv("MONGO_PASSWORD"), os.Getenv("MONGO_DB_NAME")); err != nil {
+		log.Fatal("Erro ao conectar no banco de dados:", err)
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	server := gin.Default()
+
+	server.SetTrustedProxies([]string{"127.0.0.1", "::1"})
+
+	routes.RegisterRoutes(server)
+
+	server.Run(":8080")
 }
---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rafinhacuri/api-expo-go.git/controllers"
+)
+
+func RegisterRoutes(server *gin.Engine) {
+	server.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{
+			"message": "Not Found",
+		})
+	})
+
+	path := server.Group("/api")
+
+	path.POST("/user", controllers.InsertUser)
+}
